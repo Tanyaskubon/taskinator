6149656555e7203956c71acca172a0b3d7fc4cab
Skoobieton <91810317+Tanyaskubon@users.noreply.github.com>
2021-11-28 16:27:09 -0500

    add tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,23 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler =function(event) {
     event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
     
+    //create list item
     var listItemE1 = document.createElement("li");
     listItemE1.className ="task-item";
-    listItemE1.textContent = "This is a new task.";
+   
+    // create a div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemE1.appendChild(taskInfoEl);
+
+   
+    //add entire list item to list
     tasksToDoEl.appendChild(listItemE1);
   };
 
@@ -14,7 +27,7 @@ var createTaskHandler =function(event) {
     formEl.addEventListener("submit", createTaskHandler); {   
     var listItemE1 = document.createElement("li");
     listItemE1.className = "task-item";
-    listItemE1.textContent = "This is a new task.";
+
     tasksToDoEl.appendChild(listItemE1);
    };
 
